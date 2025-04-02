c85a6ad963ef1f99a5819e109cc7889e04df3b80
lamia-alam <lamia.alam@outlook.com>
2025-04-02 11:33:54 -0400

    adding more logs

--- backend/index.js
+++ backend/index.js
@@ -6,12 +6,19 @@ const app = express();
 
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');  // Allow all domains (or specify a domain)
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');  // Allowed HTTP methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');  // Allowed headers
+  next();
+});
+
 var corsOptions = {
     origin: FRONTEND_URL,
     //optionsSuccessStatus: 200
 }
 
-console.log(FRONTEND_URL);
+console.log("CORS ALLOWING", FRONTEND_URL);
 
 app.use(cors(corsOptions));
 app.use(express.json());
