67f1c96114e23fd65d1b07055fdded6d5d7d1b27
lamia-alam <90729375+lamia-alam@users.noreply.github.com>
2025-04-03 18:24:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,22 @@
 require("dotenv").config();     
 const express = require("express");
 const cors = require("cors");
-
+const routes = require("./routes");
 
 const app = express();
 
 app.use(cors({
     origin: true,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.options('*', cors())
+//app.options('*', cors())
 
 // parse JSON bodies
 app.use(express.json());
 
-app.get('/test', (req, res) => {
-    res.header('Access-Control-Allow-Origin', 'https://csc309t11-production.up.railway.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', 'true');
-
-    res.send('Hello World');
-});
-
-const routes = require("./routes");
 // Mount your routes
 app.use("", routes);
 
