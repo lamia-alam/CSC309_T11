0eeef17779ab94a32fc9942062034cf429801bc9
lamia-alam <90729375+lamia-alam@users.noreply.github.com>
2025-04-03 15:02:22 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,28 +5,8 @@ const routes = require("./routes");
 
 const app = express();
 
-// Enable CORS for all routes
-const allowedOrigins = [
-    'http://localhost:5173', // Development frontend
-    'https://csc309t11-production.up.railway.app', // Production frontend
-    'https://backend-production-f9fa.up.railway.app' // Backend URL
-    'csc309_t11.railway.internal'
-];
-
-app.use(cors({
-    origin: function(origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
+app.options('*', cors())
 
 // parse JSON bodies
 app.use(express.json());
