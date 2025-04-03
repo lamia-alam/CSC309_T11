f8f5802bd2e09a5673e22c270ef1a639953c1d23
lamia-alam <90729375+lamia-alam@users.noreply.github.com>
2025-04-03 15:59:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,23 +6,11 @@ const routes = require("./routes");
 const app = express();
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://csc309t11-production.up.railway.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('X-Content-Type-Options', 'nosniff');
-    res.setHeader('X-Frame-Options', 'DENY');
-    res.setHeader('X-XSS-Protection', '1; mode=block');
-    res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
-    next();
-});
 
 // parse JSON bodies
 app.use(express.json());
