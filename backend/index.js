b7e0ee3bc9f1ae963a5ab08290758d64cf3679db
lamia-alam <90729375+lamia-alam@users.noreply.github.com>
2025-04-03 15:04:08 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,8 @@
 require("dotenv").config();     
 const express = require("express");
-const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
-
-app.use(cors());
-app.options('*', cors())
 
 // parse JSON bodies
 app.use(express.json());
